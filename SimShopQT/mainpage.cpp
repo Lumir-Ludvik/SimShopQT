88d3330ca86956e04065d4784943bfe2e970d72f
Lumir-Ludvik <lumirludvik@email.cz>
2020-05-04 16:25:29 +0200

    Add scalability

--- SimShopQT/mainpage.cpp
+++ SimShopQT/mainpage.cpp
@@ -37,9 +37,9 @@ void mainPage::handleSelectionChanged(const QItemSelection& selection)
 
 void mainPage::changeToFruit()
 {
-    QLabel *itemLabels[6] {ui->itemLabel, ui->itemLabel_2, ui->itemLabel_3, ui->itemLabel_4, ui->itemLabel_5, ui->itemLabel_6};
-    QLabel *priceLables[6] {ui->priceLabel, ui->priceLabel_2, ui->priceLabel_3, ui->priceLabel_4, ui->priceLabel_5, ui->priceLabel_6};
-    QLabel *goodsPics[6] {ui->goodsPic, ui->goodsPic_2, ui->goodsPic_3, ui->goodsPic_4, ui->goodsPic_5, ui->goodsPic_6};
+    QLabel *itemLabels[6] {ui->item, ui->item_2, ui->item_3, ui->item_4, ui->item_5, ui->item_6};
+    QLabel *priceLables[6] {ui->price, ui->price_2, ui->price_3, ui->price_4, ui->price_5, ui->price_6};
+    QLabel *goodsPics[6] {ui->goods, ui->goods_2, ui->goods_3, ui->goods_4, ui->goods_5, ui->goods_6};
 
     QString items[6] {"Jablko", "Banan", "Okurka", "Meloun", "Broskev", "Rajce"};
     QString prices[6] {"Cena: 28.99", "Cena: 24.99", "Cena: 15.99", "Cena: 9.5", "Cena: 53.89", "Cena: 33.5"};
@@ -58,9 +58,9 @@ void mainPage::changeToFruit()
 
 void mainPage::changeToMeat()
 {
-    QLabel *itemLabels[6] {ui->itemLabel, ui->itemLabel_2, ui->itemLabel_3, ui->itemLabel_4, ui->itemLabel_5, ui->itemLabel_6};
-    QLabel *priceLables[6] {ui->priceLabel, ui->priceLabel_2, ui->priceLabel_3, ui->priceLabel_4, ui->priceLabel_5, ui->priceLabel_6};
-    QLabel *goodsPics[6] {ui->goodsPic, ui->goodsPic_2, ui->goodsPic_3, ui->goodsPic_4, ui->goodsPic_5, ui->goodsPic_6};
+    QLabel *itemLabels[6] {ui->item, ui->item_2, ui->item_3, ui->item_4, ui->item_5, ui->item_6};
+    QLabel *priceLables[6] {ui->price, ui->price_2, ui->price_3, ui->price_4, ui->price_5, ui->price_6};
+    QLabel *goodsPics[6] {ui->goods, ui->goods_2, ui->goods_3, ui->goods_4, ui->goods_5, ui->goods_6};
 
     QString items[6] {"Hovezi jazyk", "Veprova kotleta", "Kralik cely", "Veprova krkovice", "Veprove droby", "Veprovy bok"};
     QString prices[6] {"Cena: 114.99", "Cena: 130", "Cena: 150.5", "Cena: 104", "Cena: 80.99", "Cena: 98.5"};
@@ -79,9 +79,9 @@ void mainPage::changeToMeat()
 
 void mainPage::changeToDurable()
 {
-    QLabel *itemLabels[6] {ui->itemLabel, ui->itemLabel_2, ui->itemLabel_3, ui->itemLabel_4, ui->itemLabel_5, ui->itemLabel_6};
-    QLabel *priceLables[6] {ui->priceLabel, ui->priceLabel_2, ui->priceLabel_3, ui->priceLabel_4, ui->priceLabel_5, ui->priceLabel_6};
-    QLabel *goodsPics[6] {ui->goodsPic, ui->goodsPic_2, ui->goodsPic_3, ui->goodsPic_4, ui->goodsPic_5, ui->goodsPic_6};
+    QLabel *itemLabels[6] {ui->item, ui->item_2, ui->item_3, ui->item_4, ui->item_5, ui->item_6};
+    QLabel *priceLables[6] {ui->price, ui->price_2, ui->price_3, ui->price_4, ui->price_5, ui->price_6};
+    QLabel *goodsPics[6] {ui->goods, ui->goods_2, ui->goods_3, ui->goods_4, ui->goods_5, ui->goods_6};
 
     QString items[6] {"Fazole", "Krupice", "Ryze", "Spagety", "Mouka", "Hrach"};
     QString prices[6] {"Cena: 26.5", "Cena: 20.99", "Cena: 40.25", "Cena: 23.5", "Cena: 9.89", "Cena: 21.5"};
@@ -100,9 +100,9 @@ void mainPage::changeToDurable()
 
 void mainPage::changeToDrinks()
 {
-    QLabel *itemLabels[6] {ui->itemLabel, ui->itemLabel_2, ui->itemLabel_3, ui->itemLabel_4, ui->itemLabel_5, ui->itemLabel_6};
-    QLabel *priceLables[6] {ui->priceLabel, ui->priceLabel_2, ui->priceLabel_3, ui->priceLabel_4, ui->priceLabel_5, ui->priceLabel_6};
-    QLabel *goodsPics[6] {ui->goodsPic, ui->goodsPic_2, ui->goodsPic_3, ui->goodsPic_4, ui->goodsPic_5, ui->goodsPic_6};
+    QLabel *itemLabels[6] {ui->item, ui->item_2, ui->item_3, ui->item_4, ui->item_5, ui->item_6};
+    QLabel *priceLables[6] {ui->price, ui->price_2, ui->price_3, ui->price_4, ui->price_5, ui->price_6};
+    QLabel *goodsPics[6] {ui->goods, ui->goods_2, ui->goods_3, ui->goods_4, ui->goods_5, ui->goods_6};
 
     QString items[6] {"Kofola", "Pepsi", "Rajec", "Jablecny juice", "Svijany", "Russian standard"};
     QString prices[6] {"Cena: 20.5", "Cena: 20.99", "Cena: 18.25", "Cena: 25.99", "Cena: 13", "Cena: 250"};
@@ -121,9 +121,9 @@ void mainPage::changeToDrinks()
 
 void mainPage::changeToDrugs()
 {
-    QLabel *itemLabels[6] {ui->itemLabel, ui->itemLabel_2, ui->itemLabel_3, ui->itemLabel_4, ui->itemLabel_5, ui->itemLabel_6};
-    QLabel *priceLables[6] {ui->priceLabel, ui->priceLabel_2, ui->priceLabel_3, ui->priceLabel_4, ui->priceLabel_5, ui->priceLabel_6};
-    QLabel *goodsPics[6] {ui->goodsPic, ui->goodsPic_2, ui->goodsPic_3, ui->goodsPic_4, ui->goodsPic_5, ui->goodsPic_6};
+    QLabel *itemLabels[6] {ui->item, ui->item_2, ui->item_3, ui->item_4, ui->item_5, ui->item_6};
+    QLabel *priceLables[6] {ui->price, ui->price_2, ui->price_3, ui->price_4, ui->price_5, ui->price_6};
+    QLabel *goodsPics[6] {ui->goods, ui->goods_2, ui->goods_3, ui->goods_4, ui->goods_5, ui->goods_6};
 
     QString items[6] {"Brufen 400", "Gutalax", "Paracetamol", "Simpers", "Proenzi", "Nasivin"};
     QString prices[6] {"Cena: 120.5", "Cena: 150.5", "Cena: 130", "Cena: 350.89", "Cena: 700.99", "Cena: 200.25"};
@@ -142,9 +142,9 @@ void mainPage::changeToDrugs()
 
 void mainPage::changeToSpecial()
 {
-    QLabel *itemLabels[6] {ui->itemLabel, ui->itemLabel_2, ui->itemLabel_3, ui->itemLabel_4, ui->itemLabel_5, ui->itemLabel_6};
-    QLabel *priceLables[6] {ui->priceLabel, ui->priceLabel_2, ui->priceLabel_3, ui->priceLabel_4, ui->priceLabel_5, ui->priceLabel_6};
-    QLabel *goodsPics[6] {ui->goodsPic, ui->goodsPic_2, ui->goodsPic_3, ui->goodsPic_4, ui->goodsPic_5, ui->goodsPic_6};
+    QLabel *itemLabels[6] {ui->item, ui->item_2, ui->item_3, ui->item_4, ui->item_5, ui->item_6};
+    QLabel *priceLables[6] {ui->price, ui->price_2, ui->price_3, ui->price_4, ui->price_5, ui->price_6};
+    QLabel *goodsPics[6] {ui->goods, ui->goods_2, ui->goods_3, ui->goods_4, ui->goods_5, ui->goods_6};
 
     QString items[6] {"Simeros", "Simanek", "Pirate brown", "Sim Faktor", "Simkanicek", "SimFerro"};
     QString prices[6] {"Cena: 69.69", "Cena: 22.5", "Cena: 130.99", "Cena: 120.99", "Cena: 16.5", "Cena: 34.99"};
@@ -204,50 +204,50 @@ void mainPage::on_catListView_activated(const QModelIndex &index)
     }
 }
 
-void mainPage::on_addToCartButton_clicked()
+void mainPage::on_addToCart_clicked()
 {
-    QString name = ui->itemLabel->text();
-    QString price = ui->priceLabel->text();
+    QString name = ui->item->text();
+    QString price = ui->price->text();
     QString item = name + " Kusu: 1 " + price;
     shoppingCart().SetCart(item);
 }
 
-void mainPage::on_addToCartButton_2_clicked()
+void mainPage::on_addToCart_2_clicked()
 {
-    QString name = ui->itemLabel_2->text();
-    QString price = ui->priceLabel_2->text();
+    QString name = ui->item_2->text();
+    QString price = ui->price_2->text();
     QString item = name + " Kusu: 1 " + price;
     shoppingCart().SetCart(item);
 }
 
-void mainPage::on_addToCartButton_3_clicked()
+void mainPage::on_addToCart_3_clicked()
 {
-    QString name = ui->itemLabel_3->text();
-    QString price = ui->priceLabel_3->text();
+    QString name = ui->item_3->text();
+    QString price = ui->price_3->text();
     QString item = name + " Kusu: 1 " + price;
     shoppingCart().SetCart(item);
 }
 
-void mainPage::on_addToCartButton_4_clicked()
+void mainPage::on_addToCart_4_clicked()
 {
-    QString name = ui->itemLabel_4->text();
-    QString price = ui->priceLabel_4->text();
+    QString name = ui->item_4->text();
+    QString price = ui->price_4->text();
     QString item = name + " Kusu: 1 " + price;
     shoppingCart().SetCart(item);
 }
 
-void mainPage::on_addToCartButton_5_clicked()
+void mainPage::on_addToCart_5_clicked()
 {
-    QString name = ui->itemLabel_5->text();
-    QString price = ui->priceLabel_5->text();
+    QString name = ui->item_5->text();
+    QString price = ui->price_5->text();
     QString item = name + " Kusu: 1 " + price;
     shoppingCart().SetCart(item);
 }
 
-void mainPage::on_addToCartButton_6_clicked()
+void mainPage::on_addToCart_6_clicked()
 {
-    QString name = ui->itemLabel_6->text();
-    QString price = ui->priceLabel_6->text();
+    QString name = ui->item_6->text();
+    QString price = ui->price_6->text();
     QString item = name + " Kusu: 1 " + price;
     shoppingCart().SetCart(item);
 }
--- SimShopQT/mainpage.h
+++ SimShopQT/mainpage.h
@@ -19,17 +19,17 @@ public:
 private slots:
     void on_catListView_activated(const QModelIndex &index);
 
-    void on_addToCartButton_clicked();
+    void on_addToCart_clicked();
 
-    void on_addToCartButton_2_clicked();
+    void on_addToCart_2_clicked();
 
-    void on_addToCartButton_3_clicked();
+    void on_addToCart_3_clicked();
 
-    void on_addToCartButton_4_clicked();
+    void on_addToCart_4_clicked();
 
-    void on_addToCartButton_5_clicked();
+    void on_addToCart_5_clicked();
 
-    void on_addToCartButton_6_clicked();
+    void on_addToCart_6_clicked();
 
 private:
     Ui::mainPage *ui;
